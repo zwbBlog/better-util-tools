19b5d5d8acd946627aef6c4d1d6f85c16fe7ba9b
zlgb <zhang_wenbin@aliyun.com>
2021-04-22 15:05:15 +0800

    千分位逗号格式化

--- src/index.js
+++ src/index.js
@@ -458,24 +458,10 @@
             return extended;
         },
         // 千分位逗号格式化
-        formatDecimals(n) {
-            let num = n.toString();
-            let decimals = ''
-            // 判断是否有小数
-            num.indexOf('.') > -1 ? decimals = num.split('.')[1] : decimals
-            let len = num.length
-            if (len <= 3 || num<1000) {
-                return num
-            } else {
-                let temp = ''
-                let remainder = len % 3
-                decimals ? temp = '.' + decimals : temp
-                if (remainder > 0) { // 不是3的整数倍
-                    return num.slice(0, remainder) + ',' + num.slice(remainder, len).match(/\d{3}/g).join(',') + temp
-                } else { // 是3的整数倍
-                    return num.slice(0, len).match(/\d{3}/g).join(',') + temp
-                }
-            }
+        formatDecimals(num) {
+            var source = String(num).split(".");//按小数点分成2部分
+            source[0] = source[0].replace(new RegExp('(\\d)(?=(\\d{3})+$)', 'ig'), "$1,");//只将整数部分进行都好分割
+            return source.join(".");//再将小数部分合并进来
         }
     };
     return BetterUtilTools;
