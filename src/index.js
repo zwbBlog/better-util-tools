3ca855fd3cf7a8b7669738d1da9023abfca3c14b
zhang_wenbin@aliyun.com <qq812354038>
2021-04-22 21:25:00 +0800

    千分位逗号格式化

--- src/index.js
+++ src/index.js
@@ -459,9 +459,12 @@
         },
         // 千分位逗号格式化
         formatDecimals(num) {
-            var source = String(num).split(".");//按小数点分成2部分
-            source[0] = source[0].replace(new RegExp('(\\d)(?=(\\d{3})+$)', 'ig'), "$1,");//只将整数部分进行都好分割
-            return source.join(".");//再将小数部分合并进来
+            if (this.typeIs(num) === 'number') {
+                var source = String(num).split(".");//按小数点分成2部分
+                source[0] = source[0].replace(new RegExp('(\\d)(?=(\\d{3})+$)', 'ig'), "$1,");//只将整数部分进行都好分割
+                return source.join(".");//再将小数部分合并进来
+            }
+            return num
         }
     };
     return BetterUtilTools;
