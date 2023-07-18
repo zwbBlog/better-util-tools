a2c4f7063627c179d74b9dbce3b7498de2bad96f
zlgb <zhang_wenbin@aliyun.com>
2023-07-18 13:53:23 +0800

    formatDecimals数字格式化优化

--- src/modules/common/index.js
+++ src/modules/common/index.js
@@ -497,12 +497,9 @@ export default class ICommon {
   }
   // 千分位逗号格式化
   formatDecimals(num) {
-    if (this.typeIs(num) === 'number' || this.typeIs(Number(num)) === 'number') {
-      let source = String(num).split('.');//按小数点分成2部分
-      source[0] = source[0].replace(new RegExp('(\\d)(?=(\\d{3})+$)', 'ig'), '$1,');//只将整数部分进行都好分割
-      return source.join('.');//再将小数部分合并进来
-    }
-    return num;
+    let source = String(num).split('.');//按小数点分成2部分
+    source[0] = source[0].replace(new RegExp('(\\d)(?=(\\d{3})+$)', 'ig'), '$1,');//只将整数部分进行都好分割
+    return source.join('.');//再将小数部分合并进来
   }
   //倒计时
   formatRemainTime(endTime) {
