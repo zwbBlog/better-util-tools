5ee24d52fda886e4eed5284f3219908c94679e21
zhang <zhang_wenbin@aliyun.com>
2024-02-28 17:02:16 +0800

    beta30修改

--- src/modules/common/index.js
+++ src/modules/common/index.js
@@ -846,23 +846,23 @@ export default class ICommon {
   retry(options = {}) {
     const { fn, max = 3, flag } = options;
     let retryCount = max - 1;
-    const f = flag || { key: 'status', value: 200 }
+    const mFlag = flag || { key: 'status', value: 200 }
     const fnType = this.typeIs(fn);
-    const _run = (fn, retryCount) => {
+    const _run = (f,r) => {
       return new Promise((resolve, reject) => {
         if (!['function', 'asyncfunction'].includes(fnType)) {
           return reject(`${fnType} is not function`)
         }
-        fn().then(result => {
-          if (result[f.key] === f.value) { return resolve(result) }
-          if (retryCount > 0) { return _run(fn, retryCount - 1); }
-          return reject(`Sorry, failed to retry ${max} times`)
+        f().then(result => {
+          if (result[mFlag.key] === mFlag.value) { resolve(result); return; }
+          if (r > 0) { return _run(fn, r - 1) ;}
+          reject(`Sorry, failed to retry ${max} times`)
         }).catch(e => {
-          if (retryCount > 0) { return _run(fn, retryCount - 1); }
-          return reject(JSON.stringify(e))
+          if (r > 0) { return _run(fn, r - 1); }
+          reject(JSON.stringify(e))
         })
       })
     }
-    return _run(fn, retryCount);
+    return _run(fn,retryCount);
   }
 }
